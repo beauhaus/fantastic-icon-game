587c16ab269a81a7e39f7ba5c8192aed471c8014
Beau Jackson <beau.html@gmail.com>
2019-07-28 00:56:04 -0400

    adds styled comps to GameComp

--- src/GameComponent.js
+++ src/GameComponent.js
@@ -1,11 +1,48 @@
 import React from "react";
+import styled from "styled-components";
+
+import Emoticon from "./Emoticon";
+
+const StyledGameComponent = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minMax(50px, 1fr));
+`;
+
+var emojiArray = [
+  {
+    icon_id: 0,
+    label: "Grinning Face",
+    icon: "😀"
+  },
+  {
+    icon_id: 1,
+    label: "Grinning Face With Big Eyes",
+    icon: "😃"
+  },
+  {
+    icon_id: 2,
+    label: "Grinning Face With Smiling Eyes",
+    icon: "😄"
+  },
+  {
+    icon_id: 3,
+    label: "Beaming Face With Smiling Eyes",
+    icon: "😁"
+  },
+  {
+    icon_id: 4,
+    label: "Grinning Squinting Face",
+    icon: "😆"
+  }
+];
 
 const GameComponent = () => {
-  console.log("GameComp Runs!");
   return (
-    <div class="game-comp-wrapper">
-      <h1>GameComponent</h1>
-    </div>
+    <StyledGameComponent className="game-wrapper">
+      {emojiArray.map((item, idx) => (
+        <Emoticon key={item.icon_id} label={item.label} icon={item.icon} />
+      ))}
+    </StyledGameComponent>
   );
 };
 
